c8db25e0dbae879ef15b1f3044704fab58365c3a
canberkeh <canberkehorozal@gmail.com>
2021-05-02 14:30:43 +0300

    Template update - coinrank, create_new_coin

--- coinrank/forms/new_listing_form.py
+++ coinrank/forms/new_listing_form.py
@@ -4,4 +4,7 @@ from coinrank.models import CoinRanking
 class CoinrankForm(forms.ModelForm):
     class Meta:
         model = CoinRanking
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter Name Here' }),
+        }
         exclude = ['is_approved', 'like', 'dislike', 'hodl', 'total_points', 'comment']
